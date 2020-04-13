60f4a336266312e0f6d4a8ff720e36f08d28857e
MistTheDork <54997585+MistTheDork@users.noreply.github.com>
2020-04-13 00:59:48 -0400

    Added html for the login and register pages that both are based off of the base template. Started work on index html for the blog pages. Adding in the blog for the sake of this tutorial, but this functionality will likely be removed later. Added necessary imports and blueprint registration to init. auth can now very users when they register and log in. Started on code for blog. Added a style sheet but it does not appear to work. Will try to get working later.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,5 +1,7 @@
 import functools
 
+# Blueprint for registering code and views.
+# This is for authentication.
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
@@ -8,3 +10,94 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+# allows for view and authentication. prompts user with form
+# and if invalid entry, user will get prompted again.
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+# does similar to register above, but allows an
+# existing user to log in.
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+# keeps the user logged in as they go through
+# subsequent pages.
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+# logs the user out.
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+# a decorator that requires the user to be logged in
+# to be able to access and use their info. forces the
+# user to the login page if they aren't.
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return  wrapped_view
